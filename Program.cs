a22e2b033b8446a376869b475416d962ab4cc4df
Wilfredo Velázquez-Rodríguez <zulu.inuoe@gmail.com>
2018-10-28 14:46:51 -0400

    Switch code to csharp to make it more sane and fix bugs

---
+++ Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace mrunemacs
+{
+  class Program
+  {
+    static void RunEmacsClient(string[] args)
+    {
+      var sb = new StringBuilder("-n -a \"\"");
+
+      if (args.Length > 0)
+      {
+        foreach (var arg in args)
+        {
+          sb.Append(" \"" + arg + "\"");
+        }
+      }
+      else
+      {
+        sb.Append(" -c");
+      }
+
+      string emacsClientFileName;
+
+      //If there is an emacsclient in our dir then use that, otherwise rely on PATH
+      {
+        var dir = new DirectoryInfo(AppDomain.CurrentDomain.BaseDirectory);
+        var emacsClientInDir = new FileInfo(Path.Combine(dir.FullName, "emacsclientw.exe"));
+        if (emacsClientInDir.Exists)
+        {
+          emacsClientFileName = emacsClientInDir.FullName;
+        }
+        else
+        {
+          emacsClientFileName = "emacsclientw";
+        }
+      }
+
+      var proc = new Process()
+      {
+        StartInfo = new ProcessStartInfo
+        {
+          FileName = emacsClientFileName,
+          Arguments = sb.ToString(),
+          CreateNoWindow = true,
+          WindowStyle = ProcessWindowStyle.Hidden
+        }
+      };
+
+      proc.Start();
+    }
+
+    [STAThread()]
+    static void Main(string[] args)
+    {
+      SingleInstanceApp app;
+      if (SingleInstanceApp.Aquire(out app))
+      {
+        using (app)
+        {
+          var emacsWindow = FindWindow(EmacsClassName, null);
+
+          if (args.Length == 0 && emacsWindow != IntPtr.Zero)
+          {//No args, just want to run emacs  
+           //Just pop that window to the foreground
+            if (IsIconic(emacsWindow))
+            {
+              ShowWindow(emacsWindow, SW_RESTORE);
+            }
+            SetForegroundWindow(emacsWindow);
+          }
+          else
+          {
+            RunEmacsClient(args);
+          }
+        }
+      }
+    }
+
+    [DllImport("user32.dll", SetLastError = true)]
+    static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+
+    [DllImport("user32.dll", SetLastError = true)]
+    static extern bool IsIconic(IntPtr window);
+
+    [DllImport("user32.dll", SetLastError = true)]
+    static extern bool ShowWindow(IntPtr window, int cmdShow);
+
+    [DllImport("user32.dll", SetLastError = true)]
+    static extern bool SetForegroundWindow(IntPtr window);
+
+    private const int SW_RESTORE = 9;
+    private static readonly string EmacsClassName = "Emacs";
+  }
+}
---
+++ SingleInstanceApp.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace mrunemacs
+{
+  public sealed class SingleInstanceApp : IDisposable
+  {
+    public static bool Aquire(out SingleInstanceApp app)
+    {
+      return Aquire(Assembly.GetExecutingAssembly(), out app);
+    }
+
+    public static bool Aquire(Assembly assembly, out SingleInstanceApp app)
+    {
+      // get application GUID as defined in AssemblyInfo.cs
+      string appGuid =
+          ((GuidAttribute)Assembly.GetExecutingAssembly().
+              GetCustomAttributes(typeof(GuidAttribute), false).
+                  GetValue(0)).Value.ToString();
+
+      // unique id for global mutex - Global prefix means it is global to the machine
+      string mutexId = string.Format("{{{0}}}", appGuid);
+
+      // Need a place to store a return value in Mutex() constructor call
+      bool createdNew;
+
+      // edited by MasonGZhwiti to prevent race condition on security settings via VanNguyen
+      var mutex = new Mutex(false, mutexId, out createdNew);
+      // edited by acidzombie24
+      var hasHandle = false;
+      try
+      {
+        try
+        {
+          hasHandle = mutex.WaitOne(0, false);
+        }
+        catch (AbandonedMutexException)
+        {
+          // Log the fact that the mutex was abandoned in another process,
+          // it will still get acquired
+          hasHandle = true;
+        }
+
+        if (hasHandle)
+        {
+          app = new SingleInstanceApp(mutex);
+          return true;
+        }
+        else
+        {
+          app = null;
+          return false;
+        }
+      }
+      catch
+      {
+        if (hasHandle)
+        {
+          mutex.ReleaseMutex();
+        }
+
+        app = null;
+        return false;
+      }
+    }
+
+    public void Dispose()
+    {
+      if (m_Disposed)
+      {
+        throw new ObjectDisposedException(ToString());
+      }
+
+      m_Disposed = true;
+      m_Mutex.ReleaseMutex();
+    }
+
+    private SingleInstanceApp(Mutex mutex)
+    {
+      m_Mutex = mutex;
+    }
+
+    private bool m_Disposed;
+    private readonly Mutex m_Mutex;
+  }
+}
